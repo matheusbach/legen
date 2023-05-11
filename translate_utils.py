5c59f72ef6466e8af141d4b60f6ef623a9b9b7de
matheusbach <35426162+matheusbach@users.noreply.github.com>
2023-05-11 14:03:11 -0300

    Better translation

--- translate_utils.py
+++ translate_utils.py
@@ -1,9 +1,18 @@
 import os
 import time
+import re
 
 import deep_translator
 import pysrt
 from tqdm import tqdm
+
+# all entence endings for japanese and normal people languages
+sentence_endings = ['.', '!', '?', ')', 'よ',
+                    'ね', 'の', 'さ', 'ぞ', 'な', 'か', '！', '。', '」', '…']
+
+# a good separator is a char or string that doenst change the translation quality but is near ever preserved in result at same or near position
+separator = " ◌ "
+separator_unjoin = separator.replace(' ', '')
 
 
 def translate_srt_file(srt_file_path, translated_subtitle_path, target_lang):
@@ -15,34 +24,124 @@ def translate_srt_file(srt_file_path, translated_subtitle_path, target_lang):
 
     translated_texts = []
     chunk_max_chars = 4000
-    chunks = [[]]
-    
+    chunks = []
+    unjoined_texts = []
+
     # make chunks of at maximum $chunk_max_chars to stay under Google Translate public API limits
     for line in sub_content:
-        if sum(len(s) for s in chunks[-1]) + len(line) < chunk_max_chars:
-                chunks[-1].append(line)
-        else:
-            chunks.append([line])
-    
+        chunks.append(line)
+    # join lines in each chunk
+    chunks = join_sentences(chunks, chunk_max_chars)
+
     for chunk in tqdm(chunks, desc="Translating", unit="chunks", unit_scale=True, leave=True, bar_format="{desc} {percentage:3.0f}% | ETA: {remaining}"):
         while True:
             try:
                 # Translate the subtitle content of the chunk using Google Translate
-                translated_chunk = deep_translator.GoogleTranslator(
-                    source='auto', target=target_lang).translate_batch(chunk)
-                translated_texts.extend(translated_chunk)
+                translated_texts = deep_translator.GoogleTranslator(
+                    source='auto', target=target_lang).translate(chunk)
+                
+                # Unjoin lines within each chunk that end with a sentence ending
+                unjoined_texts.extend(unjoin_sentences(chunk, translated_texts, separator_unjoin))
                 break
             except Exception as e:
-                # if an error ocurred, retry
+                # If an error occurred, retry
                 print(f"Error occurred: {e}. Retrying in 30 seconds...")
                 time.sleep(30)
 
     # Combine the original and translated subtitle content
     for i, sub in enumerate(subs):
-        sub.text = translated_texts[i]
+        sub.text = unjoined_texts[i]
 
     # Save the translated SRT file
     os.makedirs(os.path.dirname(translated_subtitle_path), exist_ok=True)
     subs.save(translated_subtitle_path, encoding='utf-8')
 
     return subs
+
+
+def join_sentences(lines, max_chars):
+    """
+    Joins the given list of strings in a way that each part ends with a sentence ending.
+    Adds a separator to all lines in the chunk.
+    """
+    joined_lines = []
+    current_chunk = ""
+
+    for line in lines:
+        if len(current_chunk) + len(line) + len(separator) <= max_chars:
+            current_chunk += line + separator
+            if any(line.endswith(ending) for ending in sentence_endings):
+                joined_lines.append(current_chunk.strip())
+                current_chunk = ""
+        else:
+            if current_chunk:
+                joined_lines.append(current_chunk.strip())
+                current_chunk = ""
+            if len(line) <= max_chars:
+                joined_lines.append(line)
+            else:
+                # Split the line into smaller chunks if it exceeds the max_chars limit
+                chunk_start = 0
+                while chunk_start < len(line):
+                    chunk_end = chunk_start + max_chars
+                    if chunk_end < len(line):
+                        # Find the last occurrence of a sentence ending within the chunk
+                        last_ending_index = max(line.rfind(ending, chunk_start, chunk_end) for ending in sentence_endings)
+                        if last_ending_index != -1:
+                            chunk_end = last_ending_index + 1
+                    joined_lines.append(line[chunk_start:chunk_end])
+                    chunk_start = chunk_end
+
+    return joined_lines
+
+def unjoin_sentences(original_sentence, modified_sentence, separator):
+    """
+    Splits the original and modified sentences into lines based on the separator.
+    Tries to match the number of lines between the original and modified sentences.
+    """
+    
+    # split by separator, remove double spaces and empty strings from list
+    original_lines = original_sentence.split(separator)
+    original_lines = [s.strip().replace('  ', ' ') for s in original_lines if s.strip()]
+    original_lines = [s for s in original_lines if s]
+    # split by separator, remove double spaces and empty strings from list
+    modified_lines = modified_sentence.split(separator)
+    modified_lines = [s.strip().replace('  ', ' ') for s in modified_lines if s.strip()]
+    modified_lines = [s for s in modified_lines if s]
+
+    # all ok, return lines
+    if len(original_lines) == len(modified_lines):
+        return modified_lines
+
+    original_word_count = sum(len(line.strip().split()) for line in original_lines)
+    modified_text = ' '.join(modified_lines)
+    modified_word_count = len(modified_text.strip().split())
+
+    # zero words? return original sentence
+    if original_word_count == 0 or modified_word_count == 0:
+        return original_sentence
+
+    # if number of lines doesnt match, e need to adjut it
+    if len(original_lines) != len(modified_lines):
+        # Join the list of strings with spaces
+        joined = ' '.join(modified_lines)
+
+        # Split the joined string into words
+        words = joined.split()
+
+        # Calculate the number of words per part
+        words_per_part = len(words) // len(original_lines)
+
+        # Split the words into parts
+        split_parts = []
+        start_index = 0
+
+        for i in range(len(original_lines) - 1):
+            end_index = start_index + words_per_part
+            split_parts.append(' '.join(words[start_index:end_index]))
+            start_index = end_index
+
+        # Add the remaining words to the last part
+        split_parts.append(' '.join(words[start_index:]))
+
+        return split_parts
